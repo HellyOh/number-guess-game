fb426182ec3513163616e9272ed4d290310312c1
hm.oh <hm.oh@taekwang.com>
2025-02-05 14:54:19 +0900

    숫자맞추기게임 3번영상

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let computerNum = 0;
 let playButton = document.querySelector("#play-button");
 let userInput = document.querySelector("#user-input");
-let result = document.querySelector("#result-area");
+let resultArea = document.querySelector("#result-area");
+let resetButton = document.querySelector("#reset-button");
+let chance = 5;
+let gameOver = false;
+let chanceArea = document.querySelector("#chance-area");
 
 // playButton 클릭 이벤트 리스너 추가
 playButton.addEventListener('click', play);
+// resetButton 클릭 이벤트 리스너 추가
+resetButton.addEventListener('click', reset);
 
 function pickRandomNumber() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -15,13 +21,39 @@ function pickRandomNumber() {
 function play() {
     let userValue = userInput.value;
     
+    chance = chance - 1;
+    chanceArea.textContent = `남은 기회: ${chance}번`;
+
+
     //정답과 비교해서 같으면 정답입니다. 다르면 두개를 비교해서 입력값이 크면 다운 작으면 업을 표시
     if (userValue == computerNum) {
-        result.textContent = "정답입니다.";
+        resultArea.textContent = "정답입니다.";
     } else if (userValue > computerNum) {
-        result.textContent = "Down";
+        resultArea.textContent = "Down";
     } else {
-        result.textContent = "Up";
+        resultArea.textContent = "Up";
+    }
+
+    //chance가 0이면 게임오버
+    if (chance == 0) {
+        gameOver = true;
+    }
+
+    if(gameOver) {
+        playButton.disabled = true;
     }
 }
+
+// reset 함수 정의
+function reset() {
+    resultArea.textContent = "결과값이 여기 나옵니다";
+    userInput.value = "";
+    pickRandomNumber();
+
+    //playButton 활성화
+    playButton.disabled = false;
+    chance = 5;
+    chanceArea.textContent = `남은 기회: ${chance}번`;
+}
+
 pickRandomNumber();
